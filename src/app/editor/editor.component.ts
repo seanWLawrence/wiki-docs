5535438f28ed5e8bee73122bcaf923087c701c5a
Sean W. Lawrence <sean.w.lawrence+programming@gmail.com>
2020-02-19 18:23:35 -0800

    Add default values for markdown and sync scrolling

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import * as marked from "marked";
 import { sanitize } from "dompurify";
+
+import DEFAULT_VALUE from "./editor.default";
 
 @Component({
   selector: "app-editor",
   templateUrl: "./editor.component.html",
   styleUrls: ["./editor.component.sass"]
 })
-export class EditorComponent implements OnInit {
+export class EditorComponent implements OnInit, OnDestroy {
   content = "";
+  scrollTop = 0;
 
   constructor() {}
 
   ngOnInit(): void {
-    this.content = window.localStorage.getItem("content") || "Some content...";
+    this.content = DEFAULT_VALUE;
+    window.addEventListener("scroll", this.scrollEvent, true);
   }
+
+  ngOnDestroy() {
+    window.removeEventListener("scroll", this.scrollEvent, true);
+  }
+
+  scrollEvent = (event: any): void => {
+    this.scrollTop = event.srcElement.scrollTop;
+  };
 
   renderMarkdown() {
     return marked(sanitize(this.content), { gfm: true });
